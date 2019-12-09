e08416ac0bb8fbb25ebf713357dc80322d7bb860
David Renshaw <dwrenshaw@gmail.com>
2019-12-09 10:16:42 -0500

    sums

--- src/jensens_inequality.lean
+++ src/jensens_inequality.lean
@@ -1,4 +1,4 @@
-import data.real.basic
+import data.real.basic data.nat.basic
 
 -- first, let's define what it means for a function to be "convex"...
 
@@ -7,3 +7,16 @@ import data.real.basic
 def is_convex (f: ℝ → ℝ) : Prop :=
   ∀ t: ℝ, ((0 ≤ t) && (t ≤ 1)) →
     ∀ x1 x2 : ℝ, f (t * x1 + (1 - t) * x2) ≤ t * f x1 + (1 - t) * f x2
+
+
+
+def sum_first_n (f: ℕ → ℝ) : ℕ → ℝ
+| 0 := f 0
+| (nat.succ n) := (f (nat.succ n)) + (sum_first_n n)
+
+def sum_seq { n : ℕ } (f: fin n → ℝ) : ℝ :=
+  let f' := λ m : ℕ, if h: m < n then f ⟨m, h⟩ else 0 in
+  sum_first_n f' n
+
+
+--def jensen_inequality {n: ℕ} (f: ℝ → ℝ) (a: fin n → 
