d23d5195ec2d9cfa24efcf7327100846c20687c8
David Renshaw <dwrenshaw@gmail.com>
2020-09-07 19:10:54 -0400

    problem statement

--- src/zeroes_ones_and_twos.lean
+++ src/zeroes_ones_and_twos.lean
@@ -2,10 +2,13 @@ import data.nat.basic
 import data.nat.digits
 
 
-def all_digits_zero_or_one : list ℕ → Prop
+def all_zero_or_one : list ℕ → Prop
 | [] := true
-| (0 :: ds) := all_digits_zero_or_one ds
-| (1 :: ds) := all_digits_zero_or_one ds
+| (0 :: ds) := all_zero_or_one ds
+| (1 :: ds) := all_zero_or_one ds
 | _ := false
 
---theorem part_one (n : ℕ) : ∃ k : ℕ, digits 10 (n * k)
+theorem part_one (n : ℕ) : ∃ k : ℕ, all_zero_or_one (digits 10 (n * k)) :=
+begin
+  sorry
+end
