625a5f35e7a93e5fb543447f1f7b4f98f5a6fdab
David Renshaw <dwrenshaw@gmail.com>
2020-09-07 12:49:26 -0400

    finset

--- src/intervals_and_distances.lean
+++ src/intervals_and_distances.lean
@@ -1,4 +1,4 @@
-import data.fintype.basic
+import data.finset.basic
 import data.set.intervals.basic
 import data.real.basic data.nat.basic
 
@@ -12,16 +12,16 @@ variables {k : Type}
 
 def unit_interval: Type := @subtype ℝ (set.Icc 0 1)
 
-structure sub_interval :=
+structure subinterval :=
 (lower_bound: unit_interval)
 (upper_bound: unit_interval)
 (well_formed: lower_bound.val ≤ upper_bound.val)
 
 
-theorem intervals_and_distances [fintype k]
-  (intervals: k → sub_interval)
-  (h: ∀ d: unit_interval, (∃ a b : ℝ, d.val = b - a) )
-  : (1 : ℝ) <  (∑ i, ((intervals i).upper_bound.val - (intervals i).lower_bound.val))  :=
+theorem intervals_and_distances
+  (intervals: finset subinterval)
+  (h: ∀ d: unit_interval, (∃ a b : ℝ, d.val = b - a /- ∧ (∃i ∈ intervals, i.val < 0  ) -/))
+  : (1 : ℝ) / intervals.card ≤ (∑ i in intervals, (i.upper_bound.val - i.lower_bound.val)) :=
 begin
   sorry
 end
