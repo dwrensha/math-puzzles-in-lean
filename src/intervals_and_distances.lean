1f687fa8308504cdc9af27517b7bcd8b3aa4e1ab
David Renshaw <dwrenshaw@gmail.com>
2020-09-07 13:51:08 -0400

    problem stated

--- src/intervals_and_distances.lean
+++ src/intervals_and_distances.lean
@@ -17,11 +17,20 @@ structure subinterval :=
 (upper_bound: unit_interval)
 (well_formed: lower_bound.val ≤ upper_bound.val)
 
+def in_subinterval (x: ℝ) (s: subinterval) : Prop :=
+  s.lower_bound.val ≤ x ∧ x ≤ s.upper_bound.val
+
+def subinterval_length (s: subinterval) : ℝ := s.upper_bound.val - s.lower_bound.val
 
 theorem intervals_and_distances
-  (intervals: finset subinterval)
-  (h: ∀ d: unit_interval, (∃ a b : ℝ, d.val = b - a /- ∧ (∃i ∈ intervals, i.val < 0  ) -/))
-  : (1 : ℝ) / intervals.card ≤ (∑ i in intervals, (i.upper_bound.val - i.lower_bound.val)) :=
+  (all_intervals: ℕ → subinterval)
+  (interval_indexes: finset ℕ)
+  (h: ∀ d: unit_interval,
+        (∃ a b : ℝ,
+          d.val = b - a
+          ∧ (∃i ∈ interval_indexes, in_subinterval a (all_intervals i))
+          ∧ (∃j ∈ interval_indexes, in_subinterval b (all_intervals j))))
+  : (1 : ℝ) / 1 ≤ (∑ i in interval_indexes, subinterval_length (all_intervals i)) :=
 begin
   sorry
 end
