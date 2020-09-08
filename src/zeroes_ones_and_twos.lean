c02d841af4789eddbfc4883ebd8e2f634c2bc093
David Renshaw <dwrenshaw@gmail.com>
2020-09-08 19:27:57 -0400

    state lemma

--- src/zeroes_ones_and_twos.lean
+++ src/zeroes_ones_and_twos.lean
@@ -1,6 +1,19 @@
+import data.int.modeq
 import data.nat.basic
 import data.pnat.basic
 import data.nat.digits
+import data.nat.gcd
+import data.zmod.basic
+
+-- let a, b, c, be natural numbers, with c < b, a and b coprime.
+-- prove that there exists k > 0 such that c a^k = c mod b.
+lemma foo
+  (a b c : ℕ)
+  (h_coprime: nat.gcd a b = 1)
+  : ∃k:ℕ+, (a^k.val) * c ≡ c [ZMOD b] :=
+begin
+  sorry,
+end
 
 
 def all_zero_or_one : list ℕ → Prop
