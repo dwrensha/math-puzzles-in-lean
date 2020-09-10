8ac3349b95f925e467b12aebbeb790e6210ece29
David Renshaw <dwrenshaw@gmail.com>
2020-09-09 20:47:35 -0400

    shorter proof from Kyle Miller on Zulip

--- src/zeroes_ones_and_twos.lean
+++ src/zeroes_ones_and_twos.lean
@@ -56,63 +56,18 @@ begin
   exact not_not.mp
 end
 
-lemma pigeonhole :
-  ∀ s: finset ℕ, ∀ f : ℕ → {x : ℕ // x ∈ s}, ∃ a b : ℕ, (a ≠ b ∧ f a = f b) :=
-begin
-  apply @finset.induction ℕ (λ s: finset ℕ, ∀ f : ℕ → {x : ℕ // x ∈ s}, ∃ a b : ℕ, (a ≠ b ∧ f a = f b)),
-  {
-     intro,
-     exfalso,
-     exact (finset.not_mem_empty (f 0).val) (f 0).property,
-  },
-  intros n s hn hip f,
-  cases classical.em (∃k:ℕ, (∀ m:ℕ, k ≤ m → (f m).val ≠ n)) with hbounded hinf,
-  {
-    obtain ⟨k0, hk0⟩ := hbounded,
-    let fk: ℕ → {x // x ∈ s} := (λn:ℕ, ⟨(f (n + k0)).val, _⟩),
-    {
-      obtain ⟨a, ha⟩ := hip fk,
-      obtain ⟨b, hb⟩ := ha,
-      use (a + k0),
-      use (b + k0),
-      cases hb,
-      split,
-      {
-        intro heq,
-        apply hb_left,
-        exact nat.add_right_cancel heq,
-      },
-      simp at hb_right,
-      exact subtype.ext hb_right,
-    },
-    have hk0a := (hk0 (n + k0)) (nat.le_add_left _ _),
-    exact finset.mem_of_mem_insert_of_ne (f (n + k0)).property hk0a,
-  },
-  {
-    have hall : ∀k:ℕ, ¬(∀ m:ℕ, k ≤ m → (f m).val ≠ n) := not_exists.mp hinf,
-    let hk0 := hall 0,
-    have : (∃ m:ℕ, ¬(0 ≤ m → (f m).val ≠ n)) := not_forall.mp hk0,
-    obtain ⟨m0, hm0⟩ := this,
-    have hnn : 0 ≤ m0 ∧ ¬ (f m0).val ≠ n := not_imp.mp hm0,
-    have hfm0 : (f m0).val = n := not_not.mp hnn.2,
 
-    let hk1 := hall (m0 + 1),
-    have : (∃ m:ℕ, ¬(m0 + 1 ≤ m → (f m).val ≠ n)) := not_forall.mp hk1,
-    obtain ⟨m1, hm1⟩ := this,
-    have hnn1 : m0 + 1 ≤ m1 ∧ ¬ (f m1).val ≠ n := not_imp.mp hm1,
-    have hfm1 : (f m1).val = n := not_not.mp hnn1.2,
-    use m0,
-    use m1,
-    split,
-    {
-      cases hnn1,
-      intro hne,
-      rw hne at hnn1_left,
-      linarith,
-    },
-    have : (f m0).val = (f m1).val := by rw [hfm0, hfm1],
-    exact subtype.eq this,
-  }
+lemma pigeonhole (s : finset ℕ) (f : ℕ → (↑s : set ℕ)) :
+  ∃ a b : ℕ, a ≠ b ∧ f a = f b :=
+begin
+  classical,
+  by_contra hc,
+  push_neg at hc,
+  have hinj : function.injective f,
+  { intros a b,
+    contrapose,
+    exact hc a b, },
+  apply not_injective_infinite_fintype f hinj,
 end
 
 lemma bar
