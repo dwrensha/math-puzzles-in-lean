7bfa38b5cc4a3fc996a439002c8530583b502f50
David Renshaw <dwrenshaw@gmail.com>
2021-01-03 14:08:07 -0500

    progress

--- src/imo2011_q3.lean
+++ src/imo2011_q3.lean
@@ -4,7 +4,7 @@ theorem imo2011Q3
   (f: ℝ → ℝ) (hf: ∀ x y, f (x + y) ≤ y * f x + f (f x))
   : (∀ x ≤ 0, f x = 0) :=
 begin
-  have hab: (∀ x t, f t ≤ t * f x - x * f x + f (f x)),
+  have hxt: (∀ x t, f t ≤ t * f x - x * f x + f (f x)),
   {
     intros x t,
     have h := (hf x (t - x)),
@@ -26,6 +26,25 @@ begin
     end,
     ring,
     exact h,
+  },
+  have hab: (∀ a b, f (f a) - f (f b) ≤ (f a) * (f b) - b * f b ∧
+                    f (f b) - f (f a) ≤ (f a) * (f b) - a * f a),
+  {
+    sorry,
+  },
+  have hab1: (∀ a b, a * f a + b * f b ≤ 2 * f a * f b),
+  {
+    intros a b,
+    have h := hab a b,
+    cases h,
+    sorry,
+  },
+  have ha: (∀ a < 0, 0 ≤ f a),
+  {
+     intros a han,
+     have h := hab1 a (2 * f a),
+     simp at h,
+     exact nonneg_of_mul_nonpos_right h han,
   },
   sorry,
 end
