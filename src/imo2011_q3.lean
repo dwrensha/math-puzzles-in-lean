96452339a0a7d14e4aa3c61c6c2b08ae2383b1bf
David Renshaw <dwrenshaw@gmail.com>
2021-01-03 11:34:07 -0500

    rewrite

--- src/imo2011_q3.lean
+++ src/imo2011_q3.lean
@@ -1,7 +1,12 @@
 import data.real.basic
 
-theorem imo2011Q3 (f: ℝ → ℝ) (hf: ∀ x y, f (x + y) ≤ y * f x + f(f x)) :
-  (∀ x ≤ 0, f x = 0) :=
+theorem imo2011Q3
+  (f: ℝ → ℝ) (hf: ∀ x y, f (x + y) ≤ y * f x + f (f x))
+  : (∀ x ≤ 0, f x = 0) :=
 begin
-  sorry
+  have hab: (∀ x t, f t ≤ t * f x - x * f x + f (f x)),
+  {
+    sorry,
+  },
+  sorry,
 end
