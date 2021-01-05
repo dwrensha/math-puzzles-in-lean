ae24811cc230cc7001be47505bd580b79384563f
David Renshaw <dwrenshaw@gmail.com>
2021-01-04 20:55:25 -0500

    prove 1 le f 1

--- src/imo2013_q5.lean
+++ src/imo2013_q5.lean
@@ -9,5 +9,12 @@ theorem imo2013Q4
   (f_iii: ∃ a, 1 < a ∧ f a = a)
   : ∀ x, 0 < x → f x = x :=
 begin
-sorry,
+  obtain ⟨a, ha1, hae⟩ := f_iii,
+  have hf1: 1 ≤ f 1,
+  {
+    have := (f_i a 1) ⟨lt_trans zero_lt_one ha1, zero_lt_one⟩,
+    rw mul_one at this,
+    exact (le_mul_iff_one_le_right (f_positive a)).mp this
+  },
+  sorry,
 end
