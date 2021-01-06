537618b812bb22b0c1d9e572177aa45d5533e055
David Renshaw <dwrenshaw@gmail.com>
2021-01-05 22:39:19 -0500

    progress

--- src/imo2013_q5.lean
+++ src/imo2013_q5.lean
@@ -5,7 +5,7 @@ theorem imo2013Q4
   (f: ℚ → ℚ)
   (f_positive: ∀ x, 0 < f x)
   (f_i: ∀ x y, (0 < x ∧ 0 < y) → f (x * y) ≤ f x * f y)
-  (f_ii: ∀ x y, (0 < x ∧ 0 < y) → f (x + y) ≤ f x + f y)
+  (f_ii: ∀ x y, (0 < x ∧ 0 < y) → f x + f y ≤ f (x + y))
   (f_iii: ∃ a, 1 < a ∧ f a = a)
   : ∀ x, 0 < x → f x = x :=
 begin
@@ -15,6 +15,18 @@ begin
     have := (f_i a 1) ⟨lt_trans zero_lt_one ha1, zero_lt_one⟩,
     rw mul_one at this,
     exact (le_mul_iff_one_le_right (f_positive a)).mp this
+  },
+  have hfn: ∀x: ℚ, (0 < x → ∀ n: ℕ, ((n: ℚ) + 1) * f x ≤ f ((n + 1) * x)),
+  {
+    intros x hx n,
+    induction n with pn hpn,
+    { simp only [one_mul, nat.cast_zero, zero_add], },
+    rw nat.cast_succ,
+    calc ((pn : ℚ) + 1 + 1) * f x = ((pn : ℚ) + 1) * f x + 1 * f x : add_mul (↑pn + 1) 1 (f x)
+        ... = (↑pn + 1) * f x + f x : by rw one_mul
+        ... ≤ f (((pn : ℚ) + 1) * x) + f x : by begin sorry, end
+        ... ≤ f (((pn : ℚ) + 1) * x + x) : sorry
+        ... = f (((pn : ℚ) + 1 + 1) * x) : sorry,
   },
   sorry,
 end
