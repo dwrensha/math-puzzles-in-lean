433225a09804746814fe2d8f1b77a80c8d9249d3
David Renshaw <dwrenshaw@gmail.com>
2021-01-05 23:12:43 -0500

    fill in some holes

--- src/imo2013_q5.lean
+++ src/imo2013_q5.lean
@@ -22,11 +22,16 @@ begin
     induction n with pn hpn,
     { simp only [one_mul, nat.cast_zero, zero_add], },
     rw nat.cast_succ,
+    have hpp : 0 < (↑pn + 1) * x,
+    {
+       sorry,
+    },
     calc (↑pn + 1 + 1) * f x = ((pn : ℚ) + 1) * f x + 1 * f x : add_mul (↑pn + 1) 1 (f x)
         ... = (↑pn + 1) * f x + f x : by rw one_mul
-        ... ≤ f ((↑pn + 1) * x) + f x : sorry
-        ... ≤ f ((↑pn + 1) * x + x) : sorry
-        ... = f ((↑pn + 1 + 1) * x) : sorry,
+        ... ≤ f ((↑pn + 1) * x) + f x : add_le_add_right hpn (f x)
+        ... ≤ f ((↑pn + 1) * x + x) : f_ii ((↑pn + 1) * x) x ⟨hpp, hx⟩
+        ... = f ((↑pn + 1) * x + 1 * x) : by rw one_mul
+        ... = f ((↑pn + 1 + 1) * x) : congr_arg f (add_mul (↑pn + 1) 1 x).symm
   },
   sorry,
 end
