e206bf75ca0b071f222b64dbfbd4a96a250fc2a3
David Renshaw <dwrenshaw@gmail.com>
2021-01-09 17:41:56 -0500

    fill in sorry

--- src/imo2013_q5.lean
+++ src/imo2013_q5.lean
@@ -96,7 +96,12 @@ begin
      { rwa ho at hx0 },
 
      have hxmfx : 0 < (x - ⌊x⌋) := by linarith,
-     have h0fx : 0 < (⌊x⌋:ℚ) := sorry,
+     have h0fx : 0 < (⌊x⌋:ℚ),
+     {
+       calc (0:ℚ) < 1 : zero_lt_one
+              ... = (int.has_one.one : ℚ) : by simp only [int.cast_one]
+              ... ≤ ⌊x⌋ : int.cast_le.mpr (le_floor.mpr hx),
+     },
 
      calc ((x - 1):ℝ) <  f ⌊x⌋ : hx0
                   ... < f (x - ⌊x⌋) + f ⌊x⌋ : lt_add_of_pos_left (f ↑⌊x⌋) (hqp (x - ↑⌊x⌋) hxmfx)
