e152b6a4890cab2331e3799f256a74ce37ba4e74
David Renshaw <dwrenshaw@gmail.com>
2021-01-21 19:11:55 -0500

    simplify and clarify imo2011_q3.lean

--- src/imo2011_q3.lean
+++ src/imo2011_q3.lean
@@ -5,84 +5,63 @@ Direct translation of the solution found in https://www.imo-official.org/problem
 -/
 
 theorem imo2011_q3
-  (f: ℝ → ℝ) (hf: ∀ x y, f (x + y) ≤ y * f x + f (f x))
-  : (∀ x ≤ 0, f x = 0) :=
+  (f : ℝ → ℝ) (hf : ∀ x y, f (x + y) ≤ y * f x + f (f x))
+  : ∀ x ≤ 0, f x = 0 :=
 begin
-  have hxt: (∀ x t, f t ≤ t * f x - x * f x + f (f x)),
-  {
-    intros x t,
-    have h := (hf x (t - x)),
-    rw (add_eq_of_eq_sub' rfl) at h,
-    linarith,
-  },
-  have hab: (∀ a b, f (f a) - f (f b) ≤ (f a) * (f b) - b * f b ∧
-                    f (f b) - f (f a) ≤ (f a) * (f b) - a * f a),
-  {
-     intros a b,
-     have h1 := hxt b (f a),
-     have h2 := hxt a (f b),
-     split; linarith,
-  },
-  have hab1: (∀ a b, a * f a + b * f b ≤ 2 * f a * f b),
-  {
-    intros a b,
-    have h := hab a b,
-    linarith,
-  },
-  have ha: (∀ a < 0, 0 ≤ f a),
-  {
-     intros a han,
-     have h := hab1 a (2 * f a),
-     simp only [add_le_iff_nonpos_left] at h,
-     exact nonneg_of_mul_nonpos_right h han,
-  },
-  have hx: (∀ x, f x ≤ 0),
-  {
-    intros x,
+  -- reparameterize
+  have hxt : (∀ x t, f t ≤ t * f x - x * f x + f (f x)),
+  { intros x t,
+    calc f t = f (x + (t - x))             : by rw (add_eq_of_eq_sub' rfl)
+         ... ≤ (t - x) * f x + f (f x)     : hf x (t - x)
+         ... = t * f x - x * f x + f (f x) : by rw sub_mul },
+
+  have hab : (∀ a b, f (f a) - f (f b) ≤ (f a) * (f b) - b * f b ∧
+                     f (f b) - f (f a) ≤ (f a) * (f b) - a * f a),
+  { intros a b,
+    split; linarith [hxt b (f a), hxt a (f b)] },
+
+  have hab_combined : (∀ a b, a * f a + b * f b ≤ 2 * f a * f b),
+  { intros a b,
+    linarith [hab a b] },
+
+  have ha : (∀ a < 0, 0 ≤ f a),
+  {  intros a han,
+     suffices : a * f a ≤ 0, from nonneg_of_mul_nonpos_right this han,
+     exact add_le_iff_nonpos_left.mp (hab_combined a (2 * f a)) },
+
+  have h_fx_nonpos : (∀ x, f x ≤ 0),
+  { intros x,
     by_contra,
     have hp : 0 < f x := not_le.mp h,
     let s := ((x * f x - f (f x)) / (f x)),
-    have h' := hxt x (min 0 s - 1),
-    have hm : min 0 s - 1 < s,
-    {
-      have := min_le_right 0 s,
-      linarith,
-    },
-    have hml : min 0 s - 1 < 0,
-    {
-      have := min_le_left 0 s,
-      linarith,
-    },
+    have hm : min 0 s - 1 < s := by linarith [min_le_right 0 s],
+    have hml : min 0 s - 1 < 0 := by linarith [min_le_left 0 s],
 
-    have hm1 : (min 0 s - 1) * f x < s * f x := (mul_lt_mul_right hp).mpr hm,
-
-    have hmz: f (min 0 s - 1) < 0 :=
+    have hmz : f (min 0 s - 1) < 0 :=
       calc f (min 0 s - 1)
-           ≤ (min 0 s - 1) * f x - x * f x + f (f x) : h'
-      ...  < s * f x - x * f x + f (f x) : by linarith
+           ≤ (min 0 s - 1) * f x - x * f x + f (f x) : hxt x (min 0 s - 1)
+      ...  < s * f x - x * f x + f (f x) : by linarith [(mul_lt_mul_right hp).mpr hm]
       ...  = 0 : by rw ((eq_div_iff (ne.symm (ne_of_lt hp))).mp rfl); linarith,
 
     have hmp : 0 ≤ f (min 0 s - 1) := ha (min 0 s - 1) hml,
-    linarith,
-  },
-  have hn: (∀x < 0, f x = 0),
-  {
-    intros x hxz,
-    exact le_antisymm (hx x) (ha x hxz)
-  },
-  have hz: f 0 = 0,
-  {
-    have hno := hn (-1) neg_one_lt_zero;
+    linarith },
+
+  have h_fx_zero_of_neg : (∀ x < 0, f x = 0),
+  { intros x hxz,
+    exact le_antisymm (h_fx_nonpos x) (ha x hxz) },
+
+  have h_zero_of_zero : f 0 = 0,
+  { suffices : 0 ≤ f 0, from le_antisymm (h_fx_nonpos 0) this,
+    have hno := h_fx_zero_of_neg (-1) neg_one_lt_zero,
     have hp := hxt (-1) (-1),
     rw hno at hp,
-    simp only [sub_zero, zero_add, mul_zero] at hp,
-    exact le_antisymm (hx 0) hp,
-  },
+    linarith },
+
   intros x hx,
   have hcases : x < 0 ∨ x = 0 := lt_or_eq_of_le hx,
+
   cases hcases,
-  {
-    exact hn x hcases,
-  },
-  rwa hcases,
+  { exact h_fx_zero_of_neg x hcases },
+  rw hcases,
+  exact h_zero_of_zero
 end
