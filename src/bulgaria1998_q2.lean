ec3c6ab1b111c438b20f81754faf381a5f940c0e
David Renshaw <dwrenshaw@gmail.com>
2021-04-25 17:02:06 -0400

    update mathlib and fix H2 in bulgaria1998_q2

--- src/bulgaria1998_q2.lean
+++ src/bulgaria1998_q2.lean
@@ -1,5 +1,6 @@
 import analysis.normed_space.inner_product
 import geometry.euclidean.basic
+import geometry.euclidean.triangle
 
 /-
 Bulgarian Mathematical Olympiad 1998, Problem 2
@@ -15,10 +16,18 @@ open_locale euclidean_geometry
 
 theorem bulgaria1998_q2
     (A B C D E M: euclidean_space ℝ (fin 2))
-    (H1 : ∥A - D∥ = ∥C - D∥)
-    (H2 : ∠ B E C = ∠ D A C)
+    (H1 : dist D A = dist D C)
+    (H2 : ∠ D A B = ∠ A B C)
     (H3 : M = midpoint ℝ B C) :
     ∠ B E C = ∠ D A C :=
 begin
+  let x := ∠ D A C,
+  have : ∠ D A C = ∠ D C A := euclidean_geometry.angle_eq_angle_of_dist_eq H1,
+  let y := ∠ C A B,
+  have : ∠ A B C = x + y,
+  {
+    rw ← H2,
+    sorry, -- hm... might need the acuteness assumption, actually.
+  },
   sorry
 end
