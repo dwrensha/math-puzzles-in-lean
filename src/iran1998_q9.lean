b65ca74e440e9b22899e4e0e779a5105f5b4cf4d
David Renshaw <dwrenshaw@gmail.com>
2021-12-11 09:18:39 -0500

    add problem statement for iran1998_q9

---
+++ src/iran1998_q9.lean
@@ -0,0 +1,22 @@
+import data.real.basic
+import data.real.sqrt
+
+/-
+Iranian Mathematical Olympiad 1998, Problem 9
+
+Let x,y,z > 1 and 1/x + 1/y + 1/z = 2. Prove that
+
+  √(x + y + z) ≥ √(x - 1) + √(y - 1) + √(z - 1).
+
+-/
+
+theorem iran1998_q9
+  (x y z : ℝ)
+  (hx : 1 < x)
+  (hy : 1 < y)
+  (hz : 1 < z)
+  (h : 1/x + 1/y + 1/z = 2) :
+  real.sqrt(x - 1) + real.sqrt(y - 1) + real.sqrt(z - 1) ≤ real.sqrt (x + y + z) :=
+begin
+  sorry
+end
