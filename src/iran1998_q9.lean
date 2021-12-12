1a0ae9eba979c44079eb8cf2ec5e015886e45c08
David Renshaw <dwrenshaw@gmail.com>
2021-12-12 14:38:41 -0500

    trying to use cauchy-schwarz

--- src/iran1998_q9.lean
+++ src/iran1998_q9.lean
@@ -1,6 +1,9 @@
 import data.real.basic
 import data.real.sqrt
 
+import analysis.inner_product_space.pi_L2
+import analysis.normed_space.pi_Lp
+
 /-
 Iranian Mathematical Olympiad 1998, Problem 9
 
@@ -9,6 +12,13 @@ Let x,y,z > 1 and 1/x + 1/y + 1/z = 2. Prove that
   √(x + y + z) ≥ √(x - 1) + √(y - 1) + √(z - 1).
 
 -/
+
+def construct_vector (x y z : ℝ) : euclidean_space ℝ (fin 3)
+| ⟨0, _⟩ := x
+| ⟨1, _⟩ := y
+| ⟨2, _⟩ := z
+| _ := 0 -- impossible
+
 
 theorem iran1998_q9
   (x y z : ℝ)
@@ -34,9 +44,14 @@ begin
   have hyf : (y-1)/y = 1 - 1/y := by field_simp,
   have hzf : (z-1)/z = 1 - 1/z := by field_simp,
 
-  have : (x-1)/x + (y-1)/y + (z-1)/z = 1,
+  have h1 : (x-1)/x + (y-1)/y + (z-1)/z = 1,
   { rw[hxf, hyf, hzf],
     linarith },
+
+  let v₁ := construct_vector (real.sqrt x) (real.sqrt y) (real.sqrt z),
+  let v₂ := construct_vector (real.sqrt ((x - 1)/x)) (real.sqrt ((y-1)/y)) (real.sqrt ((z-1)/z)),
+
+  --have : abs_inner_le_norm v₁ v₂,
 
   sorry
 end
