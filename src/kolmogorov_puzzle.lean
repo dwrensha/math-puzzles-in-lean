b1aef709c966d421df50ebd6dbf1cabfdc6f0133
David Renshaw <dwrenshaw@gmail.com>
2021-12-27 09:11:47 -0500

    curry and add problem reference

--- src/kolmogorov_puzzle.lean
+++ src/kolmogorov_puzzle.lean
@@ -7,9 +7,15 @@ import tactic.suggest
 
 /-
 
-Suppose each (finite) word is either "decent" or "indecent". Given an infinite
-sequence of characters, can you always break it into finite words so that all
-of them except perhaps the first one belong to the same class?
+Puzzle referenced from this tweet: https://twitter.com/sigfpe/status/1474173467016589323
+
+From the book _Out of their Minds: The Lives and Discoveries of 15 Great Computer Scientists_
+by Dennis Shasha and Cathy Lazere.
+
+
+Problem: Suppose each (finite) word is either "decent" or "indecent". Given an infinite
+sequence of characters, can you always break it into finite words so that all of them
+except perhaps the first one belong to the same class?
 
 Answer: yes.
 
@@ -18,16 +24,17 @@ Answer: yes.
 variable {α : Type}
 
 def break_into_words :
-   (stream ℕ) × -- word lengths
+   (stream ℕ) → -- word lengths
    (stream α) → -- original sequence
    (stream (list α)) -- sequence of words
- := @stream.corec
-     (stream ℕ × stream α)
-     (list α)
-     (λ ⟨lengths, seq⟩, stream.take (lengths.head) seq)
-     (λ ⟨lengths, seq⟩,
-          ⟨lengths.tail,
-           stream.drop (lengths.head) seq⟩)
+ := function.curry
+     (@stream.corec
+       (stream ℕ × stream α)
+       (list α)
+       (λ ⟨lengths, seq⟩, stream.take (lengths.head) seq)
+       (λ ⟨lengths, seq⟩,
+            ⟨lengths.tail,
+             stream.drop (lengths.head) seq⟩))
 
 -- #eval (stream.take 10 (break_into_words ⟨id, id⟩))
 
@@ -47,7 +54,7 @@ theorem kolmogorov_puzzle
   : (∃ (b : stream ℕ),
      (stream.all (λ x, 0 < x) b ∧
       all_same_class
-       (stream.tail $ break_into_words ⟨b, a⟩) is_decent)) :=
+       (stream.tail $ break_into_words b a) is_decent)) :=
 begin
   let p : Prop :=
      (∃ (n : ℕ), ∀ (k : ℕ), n < k →
