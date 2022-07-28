5fd3e98b3be99d748a7d0b0121d15abba5099854
David Renshaw <dwrenshaw@gmail.com>
2022-07-28 12:09:53 -0400

    [iran1998_q3] more progress

--- src/iran1998_q3.lean
+++ src/iran1998_q3.lean
@@ -16,19 +16,27 @@ Prove that
 
 -/
 
+lemma cube_root_cube (x : ℝ) (h: 0 ≤ x): (x^(3:ℝ)) ^ ((1:ℝ)/3) = x :=
+begin
+  rw[←real.rpow_mul h],
+  have : 3 * ((1:ℝ)/3) = 1 := by norm_num,
+  rw[this],
+  exact real.rpow_one x,
+end
+
 theorem iran1998_q3
   (x : ℕ → ℝ)
   (x_positive : ∀ i, 0 < x i)
   (h : ∏ (i : ℕ) in finset.range 4, x i = 1)
   : max (∑(i : ℕ) in finset.range 4, x i) (∑(i : ℕ) in finset.range 4, 1 / x i)
-     ≤ ∑ (i : ℕ) in finset.range 4, (x i)^3 :=
+     ≤ ∑ (i : ℕ) in finset.range 4, (x i)^(3 : ℝ) :=
 begin
-  let A := ∑ (i : ℕ) in finset.range 4, (x i)^3,
-  let B : ℕ → ℝ := λ j, (∑ (i : ℕ) in (finset.range 4).erase j, (x i)^3),
+  let A := ∑ (i : ℕ) in finset.range 4, (x i)^(3:ℝ),
+  let B : ℕ → ℝ := λ j, (∑ (i : ℕ) in (finset.range 4).erase j, (x i)^(3:ℝ)),
   have hab : A = (1/3) * (∑ (i : ℕ) in finset.range 4, B i),
   { simp[finset.sum_range_succ, A],
     ring },
-  have : ∀ j ∈ (finset.range 4), ∏ (i : ℕ) in (finset.range 4).erase j, x i ≤ (1/3) * B j,
+  have h2 : ∀ j ∈ (finset.range 4), ∏ (i : ℕ) in (finset.range 4).erase j, x i ≤ (1/3) * B j,
   { intros j hj,
     have hcard1: (finset.range 4).card = 4 := finset.card_range 4,
     have hcard: ((finset.range 4).erase j).card = (finset.range 4).card - 1 :=
@@ -38,12 +46,25 @@ begin
 
     have amgm := real.geom_mean_le_arith_mean_weighted
                   ((finset.range 4).erase j)
-                  (λ ii, 1/3)
-                  (λ ii, x ii ^ 3)
+                  (λ ii, (1:ℝ)/3)
+                  (λ ii, x ii ^ (3:ℝ))
                   (by { intros i hi, simp, norm_num})
                   (by { simp[finset.sum_range_succ, hcard]})
-                  (by {intros i _, simp, exact le_of_lt (x_positive i)}),
-    sorry
+                  (by {intros i _, simp, sorry }),--exact le_of_lt (x_positive i)}),
+    have hr : ∀ i ∈ ((finset.range 4).erase j),
+                 (λ (ii : ℕ), x ii ^ (3:ℝ)) i ^ (λ (ii : ℕ), (1:ℝ) / 3) i = x i,
+    { intros i _, exact cube_root_cube _ (le_of_lt (x_positive i)) },
+    rw [finset.prod_congr rfl hr] at amgm,
+    have hs : ∀ i ∈ ((finset.range 4).erase j),
+      (λ (ii : ℕ), (1:ℝ) / 3) i * (λ (ii : ℕ), x ii ^ (3:ℝ)) i =
+       ((1:ℝ)/3) * x i ^ (3:ℝ) := by simp,
+    have := finset.sum_congr rfl hs,
+    rw [finset.sum_congr rfl hs] at amgm,
+    rw [←finset.mul_sum] at amgm,
+    have hrfl : (λ (j : ℕ), ∑ (i : ℕ) in (finset.range 4).erase j, x i ^ (3:ℝ)) j =
+                  ∑ (i : ℕ) in (finset.range 4).erase j, x i ^ (3:ℝ) := rfl,
+    rw [←hrfl] at amgm,
+    exact amgm,
   },
   sorry
 end
