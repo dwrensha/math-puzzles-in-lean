74e53440556a5fd4f455525ba7e322484ed502d3
David Renshaw <dwrenshaw@gmail.com>
2022-07-27 22:43:15 -0400

    [iran1998_q3] progress

--- src/iran1998_q3.lean
+++ src/iran1998_q3.lean
@@ -1,5 +1,7 @@
 import data.real.basic
 
+import analysis.mean_inequalities
+
 open_locale big_operators
 
 /-
@@ -22,9 +24,26 @@ theorem iran1998_q3
      ≤ ∑ (i : ℕ) in finset.range 4, (x i)^3 :=
 begin
   let A := ∑ (i : ℕ) in finset.range 4, (x i)^3,
-  let B : ℕ → ℝ := λ i, A - (x i)^3,
+  let B : ℕ → ℝ := λ j, (∑ (i : ℕ) in (finset.range 4).erase j, (x i)^3),
   have hab : A = (1/3) * (∑ (i : ℕ) in finset.range 4, B i),
-  { simp[finset.sum_range_succ, B, A],
+  { simp[finset.sum_range_succ, A],
     ring },
+  have : ∀ j ∈ (finset.range 4), ∏ (i : ℕ) in (finset.range 4).erase j, x i ≤ (1/3) * B j,
+  { intros j hj,
+    have hcard1: (finset.range 4).card = 4 := finset.card_range 4,
+    have hcard: ((finset.range 4).erase j).card = (finset.range 4).card - 1 :=
+      finset.card_erase_of_mem hj,
+    rw[hcard1] at hcard,
+    norm_num at hcard,
+
+    have amgm := real.geom_mean_le_arith_mean_weighted
+                  ((finset.range 4).erase j)
+                  (λ ii, 1/3)
+                  (λ ii, x ii ^ 3)
+                  (by { intros i hi, simp, norm_num})
+                  (by { simp[finset.sum_range_succ, hcard]})
+                  (by {intros i _, simp, exact le_of_lt (x_positive i)}),
+    sorry
+  },
   sorry
 end
