39b7f1d46ca3ab648a46217c6f17d8e8043a29a0
David Renshaw <dwrenshaw@gmail.com>
2022-08-05 11:21:35 -0400

    [intervals_and_distances] add comment containing informal problem statement

--- src/intervals_and_distances.lean
+++ src/intervals_and_distances.lean
@@ -5,6 +5,13 @@ import data.real.basic data.nat.basic
 
 open_locale big_operators
 
+/-
+  Let S be the union of k disjoint, closed intervals in the unit interval [0,1].
+  Suppose S has the property that for every real number d in [0,1], there are two
+  points in S at distance d. Prove that the sum of the lengths of the intervals
+  in S is at least 1/k.
+-/
+
 def unit_interval: Type := @subtype ℝ (set.Icc 0 1)
 
 structure subinterval :=
@@ -25,7 +32,8 @@ theorem intervals_and_distances
           d.val = b - a
           ∧ (∃i ∈ interval_indexes, in_subinterval a (all_intervals i))
           ∧ (∃j ∈ interval_indexes, in_subinterval b (all_intervals j))))
-  : (1 : ℝ) / interval_indexes.card  ≤ (∑ i in interval_indexes, subinterval_length (all_intervals i)) :=
+  : (1 : ℝ) / interval_indexes.card ≤
+       (∑ i in interval_indexes, subinterval_length (all_intervals i)) :=
 begin
   sorry
 end
