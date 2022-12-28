0d6fe22d8baa218303fd16b7c422ded4d3cd0689
David Renshaw <dwrenshaw@gmail.com>
2022-12-27 20:14:50 -0500

    [bulgaria1998_q8] progress

--- src/bulgaria1998_q8.lean
+++ src/bulgaria1998_q8.lean
@@ -1,4 +1,5 @@
 import algebra.ring.basic
+import tactic.ring
 
 /-
 Bulgarian Mathematical Olympiad 1998, Problem 8
@@ -11,14 +12,68 @@ Prove that Pₙ(x,y) = Pₙ(y,x) for all x,y,n.
 
 -/
 
-variables {R : Type} [ring R]
+variables {R : Type} [comm_ring R]
 
 def P : ℕ → R → R → R
 | 0 _ _ := 1
-| (n+1) x y := (x + y - 1) * (y + 1) * P n x (y + 1) + (y - y^2) * P n x y
+| (n+1) x y := (x + y - 1) * (y + 1) * P n x (y + 2) + (y - y^2) * P n x y
+
+-- helper function
+/- Sₙ₋₁(x,y) = [(x + y)² - 1](y + 1)(x + 1)Pₙ₋₁(y+2, x).
+-/
+def S : ℕ → R → R → R
+| n x y := ((x + y)^2 - 1) * (y + 1) * (x + 1) * P n (y + 2) x
 
 theorem bulgaria1998_q8 (n : ℕ) (x y : R) : P n x y = P n y x :=
 begin
-  sorry
+  -- We induct on n. For n = 1,2 the result is evident.
+  -- So we take n > 1 and suppose that the result is true for
+  -- Pₙ₋₁(x,y) and Pₙ₋₂(x,y).
+  revert x y,
+  induction n using nat.strong_induction_on with n ih,
+  { cases n, { intros, refl },
+    cases n,
+    { intros, unfold P, ring },
+    /- We have
+       Pₙ₊₁(x,y) = (x + y - 1)(y + 1)Pₙ(x,y+2) + (y - y²)Pₙ(x,y)
+                 = (x + y - 1)(y + 1)Pₙ(y+2,x) + (y - y²)Pₙ(y,x)
+    -/
+
+    have ih1 := ih n.succ (lt_add_one (nat.succ n)),
+    have h1 : ∀ x y : R, P (n.succ + 1) x y =
+               (x + y - 1) * (y + 1) * (P n.succ (y + 2) x) +
+                   (y - y^2) * (P n.succ y x),
+    { intros x y,
+      calc P (n.succ + 1) x y
+               = (x + y - 1) * (y + 1) * (P n.succ x (y + 2)) +
+                   (y - y^2) * (P n.succ x y) : rfl
+           ... = (x + y - 1) * (y + 1) * (P n.succ (y + 2) x) +
+                   (y - y^2) * (P n.succ y x) : by {rw[ih1 x y,ih1 x (y+2)]}},
+
+    /- Note that
+      (x + y - 1)(y + 1)Pₙ(y + 2, x)
+        = Sₙ₋₁(x,y) + (x + y - 1)(y + 1)(x - x²)Pₙ₋₁(y+2,x),
+      where Sₙ₋₁(x,y) = [(x + y)² - 1](y + 1)(x + 1)Pₙ₋₁(y+2,x).
+    -/
+    have h2 : ∀ x y : R, (x + y - 1) * (y + 1) * P n.succ (y + 2) x
+        = S n x y + (x + y - 1)* (y + 1) * (x - x^2)* P n (y+2) x,
+    {intros x y,
+     unfold S,
+     unfold P,
+     ring_nf,
+     sorry},
+
+/-
+ [(x + y)² - 1](y + 1)(x + 1)Pₙ₋₁(y + 2, x) +
+ (x + y - 1)(y + 1)(x - x²)Pₙ₋₁(y + 2, x)
+
+ [x² + y² + 2xy - 1](y + 1)(x + 1)Pₙ₋₁(y + 2, x) +
+ (x + y - 1)(y + 1)x(1 - x)Pₙ₋₁(y + 2, x)
+
+
+-/
+
+    sorry,
+  },
 end
 
