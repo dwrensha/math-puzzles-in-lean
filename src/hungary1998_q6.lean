61f169b2a1029e0b23eff92a034a824feaae4abf
David Renshaw <dwrenshaw@gmail.com>
2023-01-03 13:08:14 -0500

    [hungary1998_q6] use superscript z in comments

--- src/hungary1998_q6.lean
+++ src/hungary1998_q6.lean
@@ -6,7 +6,7 @@ Hungarian Mathematical Olympiad 1998, Problem 6
 
 Let x, y, z be integers with z > 1. Show that
 
- (x + 1)² + (x + 2)² + ... + (x + 99)² ≠ y^z.
+ (x + 1)² + (x + 2)² + ... + (x + 99)² ≠ yᶻ.
 -/
 
 open_locale big_operators
@@ -14,7 +14,7 @@ open_locale big_operators
 theorem hungary1998_q6 (x y : ℤ) (z : ℕ) (hz : 1 < z) :
     ∑(i : ℕ) in finset.range 99, (x + i + 1)^2 ≠ y^z :=
 begin
- -- Suppose (x + 1)² + (x + 2)² + ... + (x + 99)² ≠ y^z.
+ -- Suppose (x + 1)² + (x + 2)² + ... + (x + 99)² ≠ yᶻ.
 
  -- We notice that
  -- y^z = (x + 1)² + (x + 2)² + ... + (x + 99)²
@@ -22,9 +22,8 @@ begin
  --     = 99x² + 2[(99 ⬝ 100)/2]x + (99 ⬝ 100 ⬝ 199)/6
  --     = 33(3x² + 300x + 50 ⬝ 199).
 
- -- which implies that 3∣y. Since z ≥ 2, 3²∣y^z, but 3² does not divide
+ -- which implies that 3∣y. Since z ≥ 2, 3²∣yᶻ, but 3² does not divide
  -- 33(3x² + 300x + 50 ⬝ 199), contradiction.
-
 
  -- (note that mathlib has finset.sum_range_id)
 
